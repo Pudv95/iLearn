d8650b2b99bfd8a562578add19a075395a15667b
Pudv95 <vivekupadhayay18@gmail.com>
2023-10-24 12:34:20 +0530

    add basic login structure

--- lib/Screens/Authentication/Login/login_page.dart
+++ lib/Screens/Authentication/Login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:ilearn/Resources/imports.dart';
+import 'package:text_divider/text_divider.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -22,11 +23,41 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    print('andar aa gya');
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(title: const Text('hello'),),
+    return  SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(width*0.1, height*0.2, width*0.1, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('Login'),
+              Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextFormField(),
+                      TextFormField(),
+                      Align(alignment: Alignment.centerRight,child: TextButton(onPressed: (){},child: Text('forgot password'),),),
+                      SizedBox(width:double.infinity,child: ElevatedButton(onPressed: (){}, child: Text('Log in'))),
+                      TextDivider.horizontal(text: const Text('Or continue with'),thickness: 1.5),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.login)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Don\'t have account?'),
+                          TextButton(onPressed: (){}, child: Text('sign up')),
+                        ],
+                      )
+
+                    ],
+                  ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
